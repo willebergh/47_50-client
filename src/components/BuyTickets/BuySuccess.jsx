4f552033d55c60a72f6b2308c73f22262526646f
willebergh <willebergh@outlook.com>
2022-12-04 23:04:30 +0100

    made purchase success it's own page

--- src/components/BuyTickets/BuySuccess.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-const BuySuccess = () => {
-	return;
-};
-
-export default BuySuccess;
---
+++ src/components/PurchaseSuccess.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const PurchaseSuccess = () => {
+	return (
+		<div>
+			<h1>tack för ditt köp!</h1>
+		</div>
+	);
+};
+
+export default PurchaseSuccess;
