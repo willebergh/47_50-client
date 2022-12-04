5bdd5b23af605bfad3dd5efa0ad3c4a07f49456f
willebergh <willebergh@outlook.com>
2022-12-04 23:02:13 +0100

    addad new route for purchaseSuccess page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import BuyTickets from "./components/BuyTickets/BuyTickets";
+import PurchaseSuccess from "./components/PurchaseSuccess";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./reset.css";
 import "./index.css";
@@ -10,6 +11,7 @@ root.render(
 	<React.StrictMode>
 		<Router>
 			<Routes>
+				<Route path="/purchase-success" element={<PurchaseSuccess />} />
 				<Route path="/buy-ticket/:event_id" element={<BuyTickets />} />
 				<Route path="*" element={<h1>404</h1>} />
 			</Routes>
